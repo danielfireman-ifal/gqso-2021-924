b43682afff6a32d96a841412954714087f656af9
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2021-07-14 17:43:31 +0000

    Rota raiz quadrada implementada

--- calc-api/src/main/java/app/App.java
+++ calc-api/src/main/java/app/App.java
@@ -3,14 +3,13 @@ package app;
 import io.jooby.Jooby;
 
 public class App extends Jooby {
-
   {
-
+    // Declara as rotas.
     mvc(new Controller());
+    mvc(new RaizQuadrada());
   }
 
   public static void main(final String[] args) {
     runApp(args, App::new);
   }
-
 }
