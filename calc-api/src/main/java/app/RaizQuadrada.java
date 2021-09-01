6ae836578a4fc8f71d7ea02304e0af439dfca75e
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2021-09-01 17:19:17 +0000

    Leva tratamento de formatação inválida para mais perto do possível problema

--- calc-api/src/main/java/app/RaizQuadrada.java
+++ calc-api/src/main/java/app/RaizQuadrada.java
@@ -7,23 +7,23 @@ public class RaizQuadrada {
     @Path("/raiz/{op}")
     @GET
     public String rotaRaizQuadrada(@PathParam String op) {
+        double operando = 0.;
         try {
-            double operando = Double.parseDouble(op);
-
-            // O endpoint raiz quadrada vai retornar um status adequado
-            // quando o parâmetro for um número menor que zero. 
-            if (operando < 0) {
-                throw new BadRequestException(String.format("Parâmetro inválido:\"%s\"", op));    
-            }
-
-            // Converter op para double (verificar erros)
-            // Calcula a raiz quadrada
-            double sqrt = Math.sqrt(operando);
-
-            // Retorna raiz quadrada para o usuário com duas casas decimais.
-            return String.format("%.2f", sqrt);
+            operando = Double.parseDouble(op);
         } catch (NumberFormatException nfe) {
             throw new BadRequestException(String.format("Parâmetro inválido:\"%s\"", op));
         }
+        // O endpoint raiz quadrada vai retornar um status adequado
+        // quando o parâmetro for um número menor que zero. 
+        if (operando < 0) {
+            throw new BadRequestException(String.format("Parâmetro inválido:\"%s\"", op));    
+        }
+
+        // Converter op para double (verificar erros)
+        // Calcula a raiz quadrada
+        double sqrt = Math.sqrt(operando);
+
+        // Retorna raiz quadrada para o usuário com duas casas decimais.
+        return String.format("%.2f", sqrt);
     }
 }
